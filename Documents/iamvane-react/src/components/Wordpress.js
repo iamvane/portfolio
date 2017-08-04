656746d841dadb3302dab85155c2604b99abb98a
IAmVane <vane.charles24@gmail.com>
2017-08-04 11:22:59 -0400

    more css fixes in wordpress project

--- Documents/iamvane-react/src/components/Wordpress.js
+++ Documents/iamvane-react/src/components/Wordpress.js
@@ -38,7 +38,7 @@ const Wordpress = (props) => {
           <a href="http://lessmoneystress.net/" rel="noopener noreferrer" target="_blank">
             <img className="wordpress-img" src={require('./../img/less-money-stress.png')}/>
             <p>Financial coaching services to people who are experiencing money issues and financial stress.</p>
-            <span className="categories">-Blog, E-commerce, UI Design</span>
+            <span className="categories">Blog, E-commerce, UI Design</span>
           </a>
          
         </div>
@@ -47,7 +47,7 @@ const Wordpress = (props) => {
           <a href="http://bikinyoga.com/" rel="noopener noreferrer" target="_blank">
             <img className="wordpress-img" src={require('./../img/bikin-yoga-logo.png')}/>
             <p>Bikin means celebration in the langugage of various religious cults of South America and the Caribbean.</p>
-            <span className="categories">-Business Website, UI Design</span>
+            <span className="categories">Business Website, UI Design</span>
           </a>
         </div>
        
@@ -55,7 +55,7 @@ const Wordpress = (props) => {
           <a href="http://www.ossetttownjuniors.co.uk/" rel="noopener noreferrer" target="_blank">
             <img className="wordpress-img" src={require('./../img/ossett-town-logo.png')}/>
             <p>A football club that provides opportunities for boys and girls in West Yorkshire, United Kingdom.</p>
-            <span className="categories">-Modernization, Deisgn, Development</span>
+            <span className="categories">Modernization, Deisgn, Development</span>
           </a>
 
         </div>
